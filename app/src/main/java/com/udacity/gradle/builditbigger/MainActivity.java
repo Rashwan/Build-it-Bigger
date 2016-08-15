c294fbe68b17b624eb68d0b6cf3afb9ab4d50177
Rashwan <ahmed.t.rashwan@gmail.com>
2016-08-15 20:23:22 +0200

    Add GCE module

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,14 +1,26 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.rashwan.myapplication.backend.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.rashwan.jokeGenerator.JokeGenerator;
 import com.rashwan.jokedisplay.JokeDisplayActivity;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -17,6 +29,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new EndPointsAsyncTask().execute(new Pair<Context, String>(this,"Rashwan"));
     }
 
 
@@ -48,5 +61,38 @@ public class MainActivity extends ActionBarActivity {
         startActivity(displayIntent);
     }
 
+}
+class EndPointsAsyncTask extends AsyncTask<Pair<Context,String>,Void,String> {
+    private static MyApi myApiService = null;
+    private Context context;
 
+    @Override
+    protected String doInBackground(Pair<Context, String>... pairs) {
+        if (myApiService == null){
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(),null)
+            .setRootUrl("http://192.168.1.77:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            myApiService = builder.build();
+        }
+        context = pairs[0].first;
+        String name = pairs[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(context,result,Toast.LENGTH_SHORT).show();
+        Log.e("TAG",result);
+    }
 }

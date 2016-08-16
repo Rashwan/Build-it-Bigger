cf02e31a6e0710b343903b60491ef5997f06d5bb
Rashwan <ahmed.t.rashwan@gmail.com>
2016-08-16 21:13:31 +0200

    Add Instrumentation test for GCE Task

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndPointsAsyncTaskInstTest.java
@@ -0,0 +1,44 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+
+import static junit.framework.Assert.assertFalse;
+
+/**
+ * Created by rashwan on 8/16/16.
+ */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class EndPointsAsyncTaskInstTest {
+
+    @Test
+    public void GCEAsyncTask() throws Exception {
+        EndPointsAsyncTask task = new EndPointsAsyncTask(){
+            @Override
+            protected void onPostExecute(String result) {
+                //Just to override the the original implementation
+            }
+        };
+        String response = task.execute(InstrumentationRegistry.getTargetContext())
+                .get(10, TimeUnit.SECONDS);
+        String joke = "";
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            if (jsonObject.has("joke")){
+                joke = jsonObject.getString("joke");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        assertFalse(joke.isEmpty());
+    }
+}
